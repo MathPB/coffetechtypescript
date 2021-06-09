502a201f4a1a403d10159ac1d17900dec19f09a8
MPBkroton <matheus.p.barbosa@kroton.com.br>
2021-06-08 23:29:17 -0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,6 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.listen(3000 || process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server is listening on port 3000");
 });
